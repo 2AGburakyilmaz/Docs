e146ba30f87f3e0b10b4f3ded942be24bc1b4637
2AGburakyilmaz <113501324+2AGburakyilmaz@users.noreply.github.com>
2023-03-20 15:34:14 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,38 +3,51 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
+copyright = '2021'
 author = 'Graziella'
 
-release = '0.1'
-version = '0.1.0'
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'recommonmark' # Markdown desteği için ekledim
+    'sphinx.ext.extlinks',
+    'recommonmark',
 ]
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',  # .md dosyaları için markdown formatı belirtildi
-}
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
+# -- Options for HTML output ----------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# -- Options for source code -----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+# -- Options for Markdown -----------------------------------------------
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# -- Options for EPUB output ----------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_metadata = {
+    'title': 'Lumache',
+    'author': 'Graziella',
+    'publisher': 'Lumache',
+}
+
+epub_show_urls = 'no'
